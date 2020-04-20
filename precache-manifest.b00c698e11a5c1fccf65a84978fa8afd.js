fc83f43247a4b01ac3dcf2966fab4115630e99f7
asri <azrhycool@gmail.com>
2020-04-20 17:00:37 +0800

    Updates

---
+++ precache-manifest.b00c698e11a5c1fccf65a84978fa8afd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922df55d83dd00ddbf3ba687c478a587",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "d8da7ecdcb8cc5dfedbb",
+    "url": "/portofolio/static/css/main.b4df72aa.chunk.css"
+  },
+  {
+    "revision": "efd3feca167e51ff1d2d",
+    "url": "/portofolio/static/js/2.b8fa643f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portofolio/static/js/2.b8fa643f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8da7ecdcb8cc5dfedbb",
+    "url": "/portofolio/static/js/main.883b1707.chunk.js"
+  },
+  {
+    "revision": "e5a85693d8695ae03ae4",
+    "url": "/portofolio/static/js/runtime-main.8451fe9d.js"
+  },
+  {
+    "revision": "cf7264d8e85ef3550de82e3ab84bac21",
+    "url": "/portofolio/static/media/about.cf7264d8.jpg"
+  },
+  {
+    "revision": "479b9d883009356437b6d13e2700fdff",
+    "url": "/portofolio/static/media/port1.479b9d88.png"
+  },
+  {
+    "revision": "046c7d056064604d8f171821ae7a573f",
+    "url": "/portofolio/static/media/port10.046c7d05.png"
+  },
+  {
+    "revision": "5ebc21512bdbc8f738e8a992ea515c20",
+    "url": "/portofolio/static/media/port11.5ebc2151.png"
+  },
+  {
+    "revision": "efcfc105ff24662bbb647b2ccaa4b406",
+    "url": "/portofolio/static/media/port12.efcfc105.png"
+  },
+  {
+    "revision": "fc62771253ca1cff498397c5478a5a50",
+    "url": "/portofolio/static/media/port2.fc627712.png"
+  },
+  {
+    "revision": "91eb9d591a24f7d37ea213d38a03c324",
+    "url": "/portofolio/static/media/port3.91eb9d59.png"
+  },
+  {
+    "revision": "e1b4ce72625dfdad5be52bd84a97873b",
+    "url": "/portofolio/static/media/port4.e1b4ce72.png"
+  },
+  {
+    "revision": "61c0769ed028b0ce2ceb5fbdf831efbe",
+    "url": "/portofolio/static/media/port5.61c0769e.png"
+  },
+  {
+    "revision": "c18c661ad8d7c57c0315d25da469eb4b",
+    "url": "/portofolio/static/media/port6.c18c661a.png"
+  },
+  {
+    "revision": "0a2030cf253aa59ac34bb90c2064277e",
+    "url": "/portofolio/static/media/port7.0a2030cf.png"
+  },
+  {
+    "revision": "e3d81f91c03d65e5504e2ba4330326e9",
+    "url": "/portofolio/static/media/port8.e3d81f91.png"
+  },
+  {
+    "revision": "a6ff18d8aea4645dcdc1f01bbe91847f",
+    "url": "/portofolio/static/media/port9.a6ff18d8.png"
+  }
+]);
